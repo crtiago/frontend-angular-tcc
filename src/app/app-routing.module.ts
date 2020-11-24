c2721e84c92b7c647186d6f8c122ae63107532ed
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2020-11-24 13:17:49 -0300

    Alterações do layout dos botões do login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, data: { title: 'Login' }},
   {
     path: 'cadastro', component: CadastroComponent,
     resolve: {
