8c1fccce64e3d63d69f5c51d8dcba97b2fcb8b08
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2020-11-30 20:10:54 -0300

    Melhorias da responsividade da tabela

--- src/app/shared-components.module.ts
+++ src/app/shared-components.module.ts
@@ -1,12 +1,18 @@
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ConfiguracoesComponent } from "./views/utils/configuracoes/configuracoes.component";
 import { SuporteComponent } from "./views/utils/suporte/suporte.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
+
+
 @NgModule({
-  imports: [ FormsModule,ReactiveFormsModule,CommonModule],
-  declarations: [ConfiguracoesComponent,SuporteComponent],
+  imports: [NgxMaskModule.forRoot(maskConfig), FormsModule, ReactiveFormsModule, CommonModule],
+  declarations: [ConfiguracoesComponent, SuporteComponent],
   exports: [ConfiguracoesComponent, SuporteComponent]
 })
-export class SharedComponentsModule {}
+export class SharedComponentsModule { }
