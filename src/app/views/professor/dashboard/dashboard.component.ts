3215641f1c6cc19ae53e16dccb20a4198aa7512f
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2021-01-02 18:58:49 -0300

    Correção dashboard Professor

--- src/app/views/professor/dashboard/dashboard.component.ts
+++ src/app/views/professor/dashboard/dashboard.component.ts
@@ -20,6 +20,7 @@ export class DashboardComponent implements OnInit {
   carregar: boolean = false;
   salaSelecionada: any;
   simuladoSelecionado: any;
+  quantidadeResposta: number = 0;
 
   constructor(private router: Router, private toastr: ToastrService, private route: ActivatedRoute, private salasService: SalasService) {
     this.listaSalas = this.route.snapshot.data.response.Data;
@@ -47,40 +48,33 @@ export class DashboardComponent implements OnInit {
   buscarResultadoSimulado() {
     console.log(this.simuladoSelecionado)
     this.simuladoSelecionadoBoolean = true;
+    this.quantidadeResposta = Number(this.simuladoSelecionado.QuantidadeResposta);
+    console.log(this.quantidadeResposta)
   }
 
   buscarDesempenhoAlunos() {
     this.carregar = true;
-    if (this.simuladoSelecionado.QuantidadeResposta == 0) {
-      this.carregar = false;
-      this.toastr.error('Nenhum envio foi realizado para esse simulado.', '', {
-        timeOut: 2000,
-        progressBar: true,
-        progressAnimation: 'decreasing',
-      });
-    } else {
-      this.carregar = true;
-      this.salasService.buscarResultadosSalaSimuladoProf(this.simuladoSelecionado.Id).pipe(first()).subscribe(
-        listaAlunos => {
-          sessionStorage.setItem('listaAlunos', JSON.stringify(listaAlunos.Data));
-          this.router.navigateByUrl("/prof/alunossimulado");
-          this.toastr.success('Lista de alunos gerada', '', {
-            timeOut: 2000,
-            progressBar: true,
-            progressAnimation: 'decreasing',
-          });
-          this.carregar = false;
-        },
-        error => {
-          this.carregar = false;
-          error = error.toString().replace("Error:", "");
-          this.toastr.error(error, '', {
-            timeOut: 2000,
-            progressBar: true,
-            progressAnimation: 'decreasing',
-          });
+    this.salasService.buscarResultadosSalaSimuladoProf(this.simuladoSelecionado.Id).pipe(first()).subscribe(
+      listaAlunos => {
+        sessionStorage.setItem('listaAlunos', JSON.stringify(listaAlunos.Data));
+        this.router.navigateByUrl("/prof/alunossimulado");
+        this.toastr.success('Lista de alunos gerada', '', {
+          timeOut: 2000,
+          progressBar: true,
+          progressAnimation: 'decreasing',
         });
-    }
+        this.carregar = false;
+      },
+      error => {
+        this.carregar = false;
+        error = error.toString().replace("Error:", "");
+        this.toastr.error(error, '', {
+          timeOut: 2000,
+          progressBar: true,
+          progressAnimation: 'decreasing',
+        });
+      });
   }
+
 
 }
