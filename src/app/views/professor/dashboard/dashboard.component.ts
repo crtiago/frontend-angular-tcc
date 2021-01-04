62ad3bd2fbeded12489c173b17752c7d2bca0a9d
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2021-01-04 11:26:46 -0300

    Geração de PDF do desempenho do aluno

--- src/app/views/professor/dashboard/dashboard.component.ts
+++ src/app/views/professor/dashboard/dashboard.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Chart } from 'chart.js';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 import { ToastrService } from 'ngx-toastr';
 import { first } from 'rxjs/operators';
 import { SalasService } from 'src/app/_servicos/salas/salas.service';
+import * as $ from "jquery";
+
+//Varivável para habilitar e usar o jquery
+declare var $: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -54,7 +60,7 @@ export class DashboardComponent implements OnInit {
     this.simuladoSelecionadoBoolean = false;
     this.listaAlunos = [];
     this.listaSimulados = [];
-    
+
     this.salasService.buscarResultadoGeralPorSala(this.salaSelecionada.Id).pipe(first()).subscribe(
       resposta => {
         this.resultadoGeralSala = resposta.Data;
@@ -75,10 +81,9 @@ export class DashboardComponent implements OnInit {
   }
 
   buscarListaSimulados() {
-    //Atualizar para quando trocar de sala
     this.salasService.buscarSimuladosPorSala(this.salaSelecionada.Id).pipe(first()).subscribe(
       resposta => {
-        this.listaSimulados = resposta.Data;       
+        this.listaSimulados = resposta.Data;
         if (this.listaSimulados.length == 0) {
           this.nenhumSimulado = true;
         } else {
@@ -103,6 +108,7 @@ export class DashboardComponent implements OnInit {
     this.salasService.buscarResultadosSalaSimuladoProf(this.simuladoSelecionado.Id).pipe(first()).subscribe(
       listaAlunos => {
         this.listaAlunos = listaAlunos.Data;
+        console.log(listaAlunos)
       },
       error => {
         error = error.toString().replace("Error:", "");
@@ -194,6 +200,94 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  gerarPdfDesempenho() {
+    //this.textoBotao = "Gerando relatórios...";
+
+    // this.carregar = true;
+    const doc = new jsPDF('p', 'mm', 'a4');
+
+    //Titulo
+    doc.setFontSize(50);
+    doc.setFont("Courier");
+    doc.setFont("bold");
+    doc.setTextColor('rgb(47,158,64)')
+    var text = "Relatório de Desempenho"
+    var textWidth = doc.getStringUnitWidth(text) * 50 / doc.internal.scaleFactor;
+    var textOffset = (doc.internal.pageSize.width - textWidth) / 2;
+    doc.text(text, textOffset, 80);
+
+    //Tabela
+    doc.setFillColor(50, 50, 50);
+    doc.rect(textOffset + 10, 120, 30, 8);
+    doc.rect(textOffset + 10, 128, 30, 8);
+    doc.rect(textOffset + 10, 136, 30, 8);
+    doc.rect(textOffset + 10, 144, 30, 8);
+    doc.rect(textOffset + 10, 120, 160, 8);
+    doc.rect(textOffset + 10, 128, 160, 8);
+    doc.rect(textOffset + 10, 136, 160, 8);
+    doc.rect(textOffset + 10, 144, 160, 8);
+
+    //Atributos
+    doc.setFontSize(12);
+    doc.setTextColor(0, 0, 0);
+    doc.text("Professor", textOffset + 12, 125);
+    doc.text("Email", textOffset + 12, 133);
+    doc.text("Data de geração", textOffset + 12, 141);
+    doc.text("Hora de geração", textOffset + 12, 149);
+
+    doc.setTextColor(0, 0, 0);
+    doc.text(this.professor.Nome, textOffset + 42, 125);
+    doc.text(this.professor.Email, textOffset + 42, 133);
+    doc.text(this.formatDate(new Date()).toString(), textOffset + 42, 141);
+    doc.text(this.formatTime(new Date()).toString(), textOffset + 42, 149);
+    doc.addPage();
+
+    var y = 10;
+    for (let i = 0; i < this.listaAlunos.length; i++) {
+      y = y + 8;
+      //Tabela
+      doc.setFillColor(50, 50, 50);
+      //Linha Nome
+      doc.rect(textOffset + 10, y, 30, 8);
+      //Linha Desempenho Geral
+      doc.rect(textOffset + 10, y + 8, 160, 8);
+      //Linha Acertos
+      doc.rect(textOffset + 10, y + 16, 30, 8);
+      //Linha Erros
+      doc.rect(textOffset + 10, y + 24, 30, 8);
+      //doc.rect(textOffset + 10, y + 16, 30, 8);
+
+      //Linha Resposta Nome
+      doc.rect(textOffset + 10, y, 160, 8);
+      //Linha Resposta Acertos
+      doc.rect(textOffset + 10, y + 16, 160, 8);
+      //Linha Resposta Erros
+      doc.rect(textOffset + 10, y + 24, 160, 8);
+
+
+
+      //Atributos
+      doc.setFontSize(12);
+      doc.setTextColor(0,0,0);
+      doc.text('Aluno', textOffset + 12, y + 6);
+      doc.text('Desempenho Geral', textOffset + 75, y + 15);
+      doc.setTextColor(0,240,0);
+      doc.text('Acertos', textOffset + 12, y + 23);
+      doc.setTextColor(240,0,0);
+      doc.text('Erros', textOffset + 12, y + 31);
+
+      doc.setTextColor(0, 0, 0);
+      doc.text(this.listaAlunos[i].Nome, textOffset + 42, y + 6);
+      doc.text(String(this.listaAlunos[i].ResultadoGeral.Acertos), textOffset + 42, y + 23);
+      doc.text(String(this.listaAlunos[i].ResultadoGeral.Erros), textOffset + 42, y + 31);
+      y = y + 30;
+    }
+
+    doc.save('relatorio.pdf')
+
+
+  }
+
   formatDate(date) {
     var d = new Date(date),
       month = '' + (d.getMonth() + 1),
@@ -208,5 +302,12 @@ export class DashboardComponent implements OnInit {
     return [day, month, year].join('/');
   }
 
+  formatTime(date) {
+    var d = new Date(date);
+    let datetext = d.toTimeString();
+
+    datetext = datetext.split(' ')[0];
+    return datetext;
+  }
 
 }
